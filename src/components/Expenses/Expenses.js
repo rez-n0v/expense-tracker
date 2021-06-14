386113627a18a47f8e3ed1ffd4e541de330963db
Prashant <pk747858@gmail.com>
2021-06-14 11:08:03 +0530

    Expense Tracker v1.0

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    if(selectedYear === 'showAll')
+      return setFilteredYear('-1');
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    if(filteredYear === '-1')
+      return true;
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  }
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  }
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    props.onAddExpense(enteredExpenseData);
+
+    stopEditingHandler();
+  }
+
+  return (
+    <div className='new-expense'>
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
